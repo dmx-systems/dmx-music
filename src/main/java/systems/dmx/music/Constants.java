e5ad65cb95da4b9991f6a4d1a067207cdf549d3d
Jörg Richter <jri@deepamehta.de>
2021-08-01 23:42:11 +0200

    Add Constants.java

---
+++ src/main/java/systems/dmx/music/Constants.java
@@ -0,0 +1,17 @@
+package systems.dmx.music;
+
+
+
+public class Constants {
+
+    // Topic Types
+    public static final String ARTIST               = "dmx.music.artist";
+    public static final String ALBUM                = "dmx.music.album";
+    public static final String TRACK                = "dmx.music.track";
+    public static final String TRACK_TITLE          = "dmx.music.track_title";
+    public static final String PLAYLIST             = "dmx.music.playlist";
+    public static final String PLAYLIST_NAME        = "dmx.music.playlist_name";
+    public static final String PLAYLIST_DESCRIPTION = "dmx.music.playlist_description";
+    public static final String PLAYLIST_ITEM        = "dmx.music.playlist_item";
+    public static final String ITEM_DESCRIPTION     = "dmx.music.item_description";
+}
