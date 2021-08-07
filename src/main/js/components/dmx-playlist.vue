a9ba36795e69c8c9a63a0dcf24393e266ae0fe93
Jörg Richter <jri@deepamehta.de>
2021-08-07 15:24:08 +0200

    UI: Playlist Items are sortable in edit mode

--- src/main/js/components/dmx-playlist.vue
+++ src/main/js/components/dmx-playlist.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="dmx-playlist">
-    <dmx-value-renderer :object="name" :level="1" :path="[]" :context="context">
-    </dmx-value-renderer>
-    <dmx-value-renderer :object="description" :level="1" :path="[]" :context="context">
-    </dmx-value-renderer>
-    <div class="field-label">{{items.length}} items</div>
-    <ol>
-      <li v-for="item in items">
-        <dmx-playlist-item :item="item"></dmx-playlist-item>
-      </li>
-    </ol>
+    <div class="field">
+      <dmx-value-renderer :object="name" :level="1" :path="[]" :context="context"></dmx-value-renderer>
+    </div>
+    <div class="field">
+      <dmx-value-renderer :object="description" :level="1" :path="[]" :context="context"></dmx-value-renderer>
+    </div>
+    <div class="field">
+      <div class="field-label">{{items.length}} items</div>
+      <ol>
+        <template v-if="infoMode">
+          <li v-for="item in items">
+            <dmx-playlist-item :item="item"></dmx-playlist-item>
+          </li>
+        </template>
+        <draggable v-else :list="items" :animation="300">
+          <!-- 3 lines duplicated in favor of code splitting; TODO: avoid -->
+          <li v-for="item in items">
+            <dmx-playlist-item :item="item"></dmx-playlist-item>
+          </li>
+        </draggable>
+      </ol>
+    </div>
   </div>
 </template>
 
@@ -17,11 +29,16 @@
 export default {
 
   mixins: [
-    require('./mixins/object').default,       // playlist to render
+    require('./mixins/object').default,       // the playlist to render
+    require('./mixins/info-mode').default,
     require('./mixins/context').default
   ],
 
   computed: {
+
+    mode () {
+      return this.context.mode
+    },
 
     name () {
       return this.object.children['dmx.music.playlist_name']
@@ -38,17 +55,25 @@ export default {
 
   components: {
     'dmx-playlist-item': require('./dmx-playlist-item').default,
+    draggable: () => ({
+      component: import('vuedraggable' /* webpackChunkName: "vuedraggable" */),
+      loading: require('./dmx-spinner')
+    })
   }
 }
 </script>
 
 <style>
-.dmx-playlist > .dmx-value-renderer + .dmx-value-renderer,
-.dmx-playlist > .field-label {
+.dmx-playlist .field + .field {
   margin-top: var(--field-spacing);
 }
 
-.dmx-playlist > ol {
+.dmx-playlist ol {
+  margin-top: 0;
   padding-left: 30px;
+}
+
+.dmx-playlist ol li {
+  line-height: var(--line-height);
 }
 </style>
---
+++ src/main/js/components/dmx-spinner.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="dmx-spinner" v-loading="true"></div>
+</template>
+
+<style>
+.dmx-spinner {
+  height: 42px;     /* see --loading-spinner-size in element-ui/packages/theme-chalk/src/common/var.scss */
+}
+</style>
